eeff191b81dc66a35306a6aa24e0c8041364345b
CCTVwat <45027819+CCTVwat@users.noreply.github.com>
2018-11-15 15:46:57 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>เข้าสู่ระบบ</title>
+<style type="text/css">
+#form1 table tr td div strong {
+	color: #FFF;
+}
+#form1 table {
+	color: #900;
+}
+#form1 {
+	text-align: center;
+}
+</style>
+</head>
+
+<body>
+
+  <div align="center"><form action="main.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+    <table width="700" align="center" background="login_bk.jpg">
+      <tr>
+        <td height="436"> <table width="100%" border="0">
+          <tr>
+            <td width="19%" height="15">&nbsp;</td>
+            <td width="81%" colspan="2">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="32">&nbsp;</td>
+            <td width="81%" colspan="2">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="38">&nbsp;</td>
+            <td width="81%" colspan="2"><input type="text" required="required" name="user"  placeholder="username" autocomplete="off" style=" background-color:transparent ; border:none" ; /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td colspan="2">&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td colspan="2"><input  type="password" required="required" name="pass" placeholder="password" style=" background-color:transparent ; border:none" ; /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td colspan="2">&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td colspan="2"><input type="image" name="imageField" id="imageField" src="bt.png" /></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table></td>
+      </tr>
+    </table>
+  </form>
+
+</body>
+</html>
